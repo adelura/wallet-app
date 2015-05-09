2c81a463fa8697330eafa88479da7518e12992b6
Artur Delura <adelura@gmail.com>
2015-05-09 15:53:48 +0200

    Improved time and date display.

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-angular.module('wallet', ['ngRoute']).config(function ($routeProvider) {
+angular.module('wallet', ['ngRoute', 'angularMoment']).config(function ($routeProvider) {
 	var cfg = {
 		controller: 'WalletCtrl',
 		templateUrl: 'wallet-index.html'

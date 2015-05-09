db71c043cbc4a20428ee0ade4ea6b4ad5c796f5a
Artur Delura <adelura@gmail.com>
2015-05-09 13:36:32 +0200

    Added total amount calculations.

--- public/js/controllers/walletCtrl.js
+++ public/js/controllers/walletCtrl.js
@@ -9,24 +9,36 @@ angular.module('wallet').controller('WalletCtrl', function WalletCtrl($scope) {
 	// Temporary model.
 	$scope.records = [];
 
+	$scope.totalValue = 0;
+
 	$scope.addIncome = function () {
 		// @TODO: Validation.
 
 		addRecord($scope.TYPES.INCOME, $scope.income.value);
 		$scope.income.value = '';
+		$scope.totalValue = calculateTotalValue();
 	};
 
 	$scope.addOutcome = function () {
 		// @TODO: Validation.
 
 		addRecord($scope.TYPES.OUTCOME, $scope.outcome.value);
-		$scope.income.value = '';
+		$scope.outcome.value = '';
+		$scope.totalValue = calculateTotalValue();
 	};
 
+	// Returns total wallet value.
+	function calculateTotalValue() {
+		return _.reduce($scope.records, function (memo, record) {
+			return memo + (record.type === $scope.TYPES.INCOME ? record.value : (record.value * -1));
+		}, 0);
+	}
+
+	// Add record to storage.
 	function addRecord(type, value) {
 		$scope.records.push({
 			date: new Date(),
-			value: value,
+			value: Number(value),
 			type: type
 		});
 	}

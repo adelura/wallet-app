f55fa835296f9951cce18eba53b92b2a316f8bac
Artur Delura <adelura@gmail.com>
2015-05-09 12:22:20 +0200

    Added simple inserting records functionality.

--- public/js/controllers/walletCtrl.js
+++ public/js/controllers/walletCtrl.js
@@ -1,7 +1,33 @@
 /*global angular */
 
 angular.module('wallet').controller('WalletCtrl', function WalletCtrl($scope) {
-	$scope.records = [
-		{ date: new Date(), value: 100 }
-	];
+	$scope.TYPES = {
+		INCOME: 1,
+		OUTCOME: 2
+	};
+
+	// Temporary model.
+	$scope.records = [];
+
+	$scope.addIncome = function () {
+		// @TODO: Validation.
+
+		addRecord($scope.TYPES.INCOME, $scope.income.value);
+		$scope.income.value = '';
+	};
+
+	$scope.addOutcome = function () {
+		// @TODO: Validation.
+
+		addRecord($scope.TYPES.OUTCOME, $scope.outcome.value);
+		$scope.income.value = '';
+	};
+
+	function addRecord(type, value) {
+		$scope.records.push({
+			date: new Date(),
+			value: value,
+			type: type
+		});
+	}
 });

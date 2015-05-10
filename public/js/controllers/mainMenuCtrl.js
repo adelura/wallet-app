d81392d6f123d3fdf0d8b9a9d8c2a1817ab6ac60
Artur Delura <adelura@gmail.com>
2015-05-10 12:41:34 +0200

    Added responsive menu.

---
+++ public/js/controllers/mainMenuCtrl.js
@@ -0,0 +1,8 @@
+/*global angular */
+
+angular.module('wallet').controller('MainMenuController', function MainMenuController($scope) {
+	$scope.active = false;
+	$scope.toggleMenu = function() {
+		$scope.active = !$scope.active;
+	}
+});
---
+++ public/js/directives/mainMenu.js
@@ -0,0 +1,9 @@
+/*global angular */
+
+angular.module('wallet').directive('mainMenu', function () {
+
+	return {
+		restrict: 'E',
+		templateUrl: '../templates/mainMenu.html'
+	};
+});
